10e1c5c4dc0cf096ec7542e3477ac29fc13769b3
DerickFerdinands <derickandrewferdinands@gmail.com>
2022-04-28 10:41:58 +0530

    Content Updated

--- Layered_Architecture/src/dao/CustomerDAOImpl.java
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -14,7 +14,6 @@ import java.util.List;
 public class CustomerDAOImpl {
 
     public static ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
-
         ResultSet rst = CrudUtil.execute("SELECT * FROM Customer");
         ArrayList<CustomerDTO> cusList = new ArrayList<>();
         while (rst.next()) {
@@ -24,29 +23,21 @@ public class CustomerDAOImpl {
     }
 
     public static boolean saveCustomer(String id, String name, String address) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
-        pstm.setString(1, id);
-        pstm.setString(2, name);
-        pstm.setString(3, address);
-        return pstm.executeUpdate()>=1;
+        return CrudUtil.execute("INSERT INTO Customer (id,name, address) VALUES (?,?,?)",id,name,address);
     }
 
     public static boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
-        pstm.setString(1, id);
-        return pstm.executeUpdate()>=1;
+        return CrudUtil.execute("DELETE FROM Customer WHERE id=?",id);
 
     }
 
     public static boolean updateCustomer(String id , String name, String address) throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+        PreparedStatement pstm = connection.prepareStatement();
         pstm.setString(1, name);
         pstm.setString(2, address);
         pstm.setString(3, id);
-        return pstm.executeUpdate()>=1;
+        return CrudUtil.execute("UPDATE Customer SET name=?, address=? WHERE id=?")
     }
 
     public static boolean CustomerExists(String id) throws SQLException, ClassNotFoundException {
